519689d2dbfee114a91bc18afa8a17d0510202f3
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2026-03-31 16:17:16 +0545

    feat :

--- src/main/java/com/mycompany/myapp/repository/AuthorityRepository.java
+++ src/main/java/com/mycompany/myapp/repository/AuthorityRepository.java
@@ -1,11 +1,14 @@
 package com.mycompany.myapp.repository;
 
-import com.mycompany.myapp.domain.Authority;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.mycompany.myapp.domain.Authority;
 
 /**
  * Spring Data JPA repository for the Authority entity.
+ * 
+ * is used to tell the compiler/IDE to ignore warnings about unused code.
  */
 @SuppressWarnings("unused")
 @Repository
